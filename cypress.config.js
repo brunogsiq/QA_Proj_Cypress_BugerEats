16cfdb9b3be059ef777eaa8caa94c1e11370fed0
brunogsiq <brunogsiq@gmail.com>
2025-03-14 12:35:06 -0300

    pipeline test - Github Actions

--- cypress.config.js
+++ cypress.config.js
@@ -1,14 +1,22 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   viewportWidth: 1440,
   viewportHeight: 900,
+  baseUrl: 'https://buger-eats-qa.vercel.app',
+  reporter: "mochawesome",
+  reporterOptions: {
+    reportDir: "cypress/reports",  // Pasta onde os relatórios serão salvos
+    overwrite: false,
+    html: true,
+    json: true
+  },
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
       return require('./cypress/plugins/index.js')(on, config)
     },
-    baseUrl: 'https://buger-eats-qa.vercel.app',
+    
   },
-})
+});
